a76b40f2c537a1234ced6ac459cb7e810b0fcaab
RemoveFunzionaConLindiceOConLaReference <francescopiazzi25@gmail.com>
2024-05-21 08:15:17 +0200

    made random thing that works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use futures::StreamExt;
 use tracing::Instrument;
+use std::thread::sleep;
 
 use actum::prelude::*;
 
@@ -7,76 +8,55 @@ async fn generic_parent<AB>(mut cell: AB, me: ActorRef<u64>)
 where
     AB: ActorBounds<u64>,
 {
-    let m1 = cell.recv().await.unwrap();
-    tracing::info!(recv = m1);
+    loop{
+        let mut last_msg_received: Option<u64> = Option::None;
 
-    let parent = me.clone();
-    let child = cell
-        .spawn(move |cell, me| async move {
-            generic_child(cell, me, parent, m1).await;
-        })
-        .await
-        .unwrap();
-    let span = tracing::info_span!("child");
-    tokio::spawn(child.task.run_task().instrument(span));
+        // receive a message
+        let m1 = cell.recv().await.unwrap();
+        tracing::info!(recv = m1);
 
-    let m2 = cell.recv().await.unwrap();
-    tracing::info!(recv = m2);
+        let n_childs = m1;
 
-    assert_eq!(m2, m1 * 2);
+        for _ in 0..n_childs {
+            // spawn a child actor
+            let parent = me.clone();
+            let child = cell
+                .spawn(move | cell, me| async move {
+                    times_two_node(cell, me, parent, m1).await;
+                })
+                .await
+                .unwrap();
+            let span = tracing::info_span!("child");
+            tokio::spawn(child.task.run_task().instrument(span));
+        }
+
+        // wait for all children to finish
+        for _ in 0..n_childs {
+            let m2 = cell.recv().await.unwrap();
+            match last_msg_received {
+                Some(last_msg) => {
+                    if m2 == last_msg{
+                        tracing::info!("received from child: {}", m2);
+                    } else {
+                        tracing::error!("received from child: {}, expected: {}", m2, last_msg);
+                    }
+                },
+                None => {last_msg_received = Some(m2); tracing::info!("received from child: {} (first)", m2);}
+            }
+        }
+
+        tracing::info!("all children finished");
+    }
 }
 
-async fn generic_child<AB>(mut cell: AB, mut me: ActorRef<u64>, mut parent: ActorRef<u64>, m: u64)
+async fn times_two_node<AB>(mut _cell: AB, mut _me: ActorRef<u64>, mut parent: ActorRef<u64>, m: u64)
 where
     AB: ActorBounds<u64>,
 {
-    tracing::info!(try_send = m * 2);
-    me.try_send(m * 2).unwrap();
-
-    let m = cell.recv().await.unwrap();
-    tracing::info!(recv = m);
-
-    tracing::info!(try_send = m);
-    parent.try_send(m).unwrap();
+    tracing::trace!(try_send = m*2);
+    parent.try_send(m*2).unwrap();
 }
 
-#[tokio::test]
-async fn test() {
-    use actum::testkit::testkit;
-
-    tracing_subscriber::fmt()
-        .with_span_events(
-            tracing_subscriber::fmt::format::FmtSpan::NEW | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
-        )
-        .with_target(false)
-        .with_line_number(true)
-        .with_max_level(tracing::Level::TRACE)
-        .init();
-
-    let mut parent = testkit(generic_parent);
-    let span = tracing::trace_span!("parent");
-    let handle = tokio::spawn(parent.task.run_task().instrument(span));
-
-    parent.m_ref.try_send(1).unwrap();
-
-    let effect = parent.executor.next().await.unwrap().recv().unwrap();
-    assert_eq!(*effect.message().unwrap(), 1);
-    drop(effect);
-
-    let mut effect = parent.executor.next().await.unwrap().spawn().unwrap();
-    let mut child = effect.executor().unwrap().downcast::<u64>().unwrap();
-    drop(effect);
-
-    let effect = child.next().await.unwrap().recv().unwrap();
-    assert_eq!(*effect.message().unwrap(), 2);
-    drop(effect);
-
-    let effect = parent.executor.next().await.unwrap().recv().unwrap();
-    assert_eq!(*effect.message().unwrap(), 2);
-    drop(effect);
-
-    handle.await.unwrap();
-}
 
 #[tokio::main]
 async fn main() {
@@ -88,12 +68,15 @@ async fn main() {
         .with_line_number(true)
         .with_max_level(tracing::Level::INFO)
         .init();
-
     let mut parent = actum(generic_parent);
     let span = tracing::info_span!("parent");
     let handle = tokio::spawn(parent.task.run_task().instrument(span));
 
-    parent.m_ref.try_send(1).unwrap();
+    parent.m_ref.try_send(5).unwrap();
+    sleep(std::time::Duration::from_secs(1));
+    parent.m_ref.try_send(7).unwrap();
+    sleep(std::time::Duration::from_secs(1));
+    parent.m_ref.try_send(9).unwrap();
 
     handle.await.unwrap();
 }
