32c81998a16230c7bc774d6e5ebf3b92dbabee5d
RemoveFunzionaConLindiceOConLaReference <francescopiazzi25@gmail.com>
2024-06-26 12:38:43 +0200

    nvm it still doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,48 @@
-use futures::StreamExt;
 use tracing::Instrument;
-use std::{any::Any, iter::Rev, thread::{sleep, JoinHandle}};
 
 use actum::prelude::*;
-
 
 pub enum RaftMessage {
     AddPeer(ActorRef<RaftMessage>),
     RemovePeer(ActorRef<RaftMessage>),
-    AppendEntry(String),    // TODO: replace with any type that is Send + 'static
+    AppendEntry(String), // TODO: replace with any type that is Send + 'static
     AppendEntryResponse,
     RequestVote,
     RequestVoteResponse,
 }
 
-
-async fn raft_actor<AB>(mut cell: AB, me: ActorRef<RaftMessage>) 
-where AB: ActorBounds<RaftMessage>
+async fn raft_actor<AB>(mut cell: AB, _me: ActorRef<RaftMessage>)
+where
+    AB: ActorBounds<RaftMessage>,
 {
     tracing::trace!("Raft actor running");
     let mut npeers = 0;
-    loop{
-        let msg = cell.recv().await.message().unwrap();
+    loop {
+        let msg = cell.recv().await.message();
         match msg {
-            RaftMessage::AddPeer(peer) => {
-                npeers += 1;
-                tracing::info!("Peer added, total: {}", npeers);
+            Some(raftmessage) => match raftmessage {
+                RaftMessage::AddPeer(_peer) => {
+                    npeers += 1;
+                    tracing::info!("Peer added, total: {}", npeers);
+                }
+                _ => {
+                    tracing::info!("Received a message");
+                }
             },
-            _ => {
-                tracing::info!("Received a message");
+            None => {
+                tracing::info!("Received a None message, quitting");
+                break;
             }
         }
     }
 }
 
 
-// this works, but nodes aren't in a vector
-async fn raft_1(){
+// works, but servers are in normal variables
+#[allow(dead_code)]
+async fn raft_1() {
     // create a simulated environment
-    let main_node = actum(move | mut cell, _me: ActorRef<RaftMessage> | async move {     
-        let mut server1 = cell.spawn(raft_actor).await.unwrap();
-        let mut server2 = cell.spawn(raft_actor).await.unwrap();
-        let mut server3 = cell.spawn(raft_actor).await.unwrap();
-
-        tracing::info!("Servers initialized");
-        sleep(std::time::Duration::from_secs(3));
-
-        let handles = vec![
-            tokio::spawn(server1.task.run_task().instrument(tracing::info_span!("server1"))),
-            tokio::spawn(server2.task.run_task().instrument(tracing::info_span!("server2"))),
-            tokio::spawn(server3.task.run_task().instrument(tracing::info_span!("server3"))),
-        ];
-
-        tracing::info!("Handles initialized");
-        sleep(std::time::Duration::from_secs(3));
-
-        let _ = server1.m_ref.try_send(RaftMessage::AddPeer(server2.m_ref.clone()));
-        let _ = server1.m_ref.try_send(RaftMessage::AddPeer(server3.m_ref.clone()));
-        let _ = server2.m_ref.try_send(RaftMessage::AddPeer(server1.m_ref.clone()));
-        let _ = server2.m_ref.try_send(RaftMessage::AddPeer(server3.m_ref.clone()));
-        let _ = server3.m_ref.try_send(RaftMessage::AddPeer(server1.m_ref.clone()));
-        let _ = server3.m_ref.try_send(RaftMessage::AddPeer(server2.m_ref.clone()));
-        
-        tracing::info!("Neighbors initialized");
-        
-        sleep(std::time::Duration::from_secs(3));
-
-        for handle in handles {
-            handle.await.unwrap();
-        }
-    });
-
-    let handle = tokio::spawn(main_node.task.run_task().instrument(tracing::info_span!("actum")));
-    handle.await.unwrap();
-
-    tracing::info!("Main node has finished");
-}
-
-
-// this doesn't work, the first call of recv() in the raft_actor immediately returns None
-async fn raft_2(){
-    // create a simulated environment
-    let main_node = actum(move | mut cell, _me: ActorRef<RaftMessage> | async move {     
+    let main_node = actum(move |mut cell, _me: ActorRef<RaftMessage>| async move {
         let server1 = cell.spawn(raft_actor).await.unwrap();
         let server2 = cell.spawn(raft_actor).await.unwrap();
         let server3 = cell.spawn(raft_actor).await.unwrap();
@@ -94,17 +55,32 @@ async fn raft_2(){
         //         if !std::ptr::eq(server, other_server) {
         //              let _ = server.try_send(RaftMessage::AddPeer(other_server.clone()));
         let servers_ref = vec![
-            server1.m_ref.clone(), 
-            server2.m_ref.clone(), 
-            server3.m_ref.clone()
+            server1.m_ref.clone(),
+            server2.m_ref.clone(),
+            server3.m_ref.clone(),
         ];
 
         tracing::info!("Servers initialized");
 
         let handles = vec![
-            tokio::spawn(server1.task.run_task().instrument(tracing::info_span!("server1"))),
-            tokio::spawn(server2.task.run_task().instrument(tracing::info_span!("server2"))),
-            tokio::spawn(server3.task.run_task().instrument(tracing::info_span!("server3"))),
+            tokio::spawn(
+                server1
+                    .task
+                    .run_task()
+                    .instrument(tracing::info_span!("server1")),
+            ),
+            tokio::spawn(
+                server2
+                    .task
+                    .run_task()
+                    .instrument(tracing::info_span!("server2")),
+            ),
+            tokio::spawn(
+                server3
+                    .task
+                    .run_task()
+                    .instrument(tracing::info_span!("server3")),
+            ),
         ];
 
         for server_ref in &servers_ref {
@@ -123,79 +99,86 @@ async fn raft_2(){
         }
     });
 
-    let handle = tokio::spawn(main_node.task.run_task().instrument(tracing::info_span!("actum")));
+    let handle = tokio::spawn(
+        main_node
+            .task
+            .run_task()
+            .instrument(tracing::info_span!("actum")),
+    );
     handle.await.unwrap();
 
     tracing::info!("Main node has finished");
 }
 
 
-// minimal code to reproduce the issue, recv() immediately returns None, even if no message has been sent
-async fn raft_3(){
-    let main_node = actum(move | mut cell, _me: ActorRef<RaftMessage> | async move {     
-        let mut servers = Vec::new();
+// doesn't work already
+async fn raft_2() {
+    // create a simulated environment
+    let main_node = actum(move |mut cell, _me: ActorRef<RaftMessage>| async move {
+        let server1 = cell.spawn(raft_actor).await.unwrap();
+        let server2 = cell.spawn(raft_actor).await.unwrap();
+        let server3 = cell.spawn(raft_actor).await.unwrap();
 
-        for _ in 0..5 {
-            let server = cell.spawn(raft_actor).await.unwrap();
-            servers.push(server);
-        }
+        let servers = vec![server1, server2, server3];
+
+        let servers_ref = servers
+            .iter()
+            .map(|server| server.m_ref.clone())
+            .collect::<Vec<_>>();
 
         tracing::info!("Servers initialized");
-        sleep(std::time::Duration::from_secs(3));
 
-        let mut handles = Vec::new();
-        for server in servers {
-            let handle = tokio::spawn(server.task.run_task().instrument(tracing::info_span!("server")));
-            handles.push(handle);
+        let handles = servers
+            .into_iter()
+            .map(|server| {
+                tokio::spawn(
+                    server
+                        .task
+                        .run_task()
+                        .instrument(tracing::info_span!("server")),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        for server_ref in &servers_ref {
+            for other_server_ref in &servers_ref {
+                if !std::ptr::eq(server_ref, other_server_ref) {
+                    let mut server1 = server_ref.clone();
+                    let server2 = other_server_ref.clone();
+                    let _ = server1.try_send(RaftMessage::AddPeer(server2));
+                }
+            }
         }
-
-        tracing::info!("Handles initialized");
-        sleep(std::time::Duration::from_secs(3));
-
-        // get all the servers to know each other
-        // for i in 0..servers_ref.len() {
-        //     for j in 0..servers_ref.len() {
-        //         if i!=j {
-        //             // must clone the references otherwise I'd have to borrow the vector once mutably and once immutably
-        //             let server = &mut servers_ref[i].clone();
-        //             let other_server = &servers_ref[j].clone();
-        //             if !std::ptr::eq(server, other_server) {
-        //                 let _ = server.try_send(RaftMessage::AddPeer(other_server.clone()));
-        //             }
-        //         }
-        //     }
-        // }
-        
         tracing::info!("Neighbors initialized");
-        
-        sleep(std::time::Duration::from_secs(3));
 
         for handle in handles {
             handle.await.unwrap();
         }
     });
 
-    let handle = tokio::spawn(main_node.task.run_task().instrument(tracing::info_span!("actum")));
+    let handle = tokio::spawn(
+        main_node
+            .task
+            .run_task()
+            .instrument(tracing::info_span!("actum")),
+    );
     handle.await.unwrap();
 
     tracing::info!("Main node has finished");
 }
-
 
 #[tokio::main]
 async fn main() {
     // initialize program logger
     tracing_subscriber::fmt()
         .with_span_events(
-            tracing_subscriber::fmt::format::FmtSpan::NEW | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
+            tracing_subscriber::fmt::format::FmtSpan::NEW
+                | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
         )
         .with_target(false)
         .with_line_number(true)
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(tracing::Level::INFO)
         .init();
 
-
-    // raft_1().await;
     raft_2().await;
 }
-
