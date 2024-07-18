01475afcb056c3083559be074cdee38c914e17bd
RemoveFunzionaConLindiceOConLaReference <francescopiazzi25@gmail.com>
2024-07-18 17:20:56 +0200

    nodes can now detect when the leader is dead

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,21 @@
+use std::time::Duration;
+use tokio::time::timeout;
+
 use actum::prelude::*;
+
+// TODO: replace with any type that is Send + 'static
+type Message = String;
 
 pub enum RaftMessage {
     AddPeer(ActorRef<RaftMessage>),
-    AppendEntry(String), // TODO: replace with any type that is Send + 'static
-    AppendEntryResponse,
+
+    AppendEntries(Vec<Message>),
+
+    // only failure that we will answer is if we are not the leader, so we send the address of the leader back
+    AppendEntryResponse(Result<(), ActorRef<RaftMessage>>),
+
     RequestVote,
+
     RequestVoteResponse,
 }
 
@@ -14,8 +25,27 @@ where
 {
     let total_nodes = 5;
 
-    let peers = init(cell, total_nodes).await;
-    tracing::info!("peers initialized");
+    let mut log: Vec<Message> = Vec::new();
+
+    let mut commit_index = 0;
+    let mut last_applied = 0;
+
+    let mut current_term = 0;
+    let mut voted_for: Option<ActorRef<RaftMessage>> = None;
+
+    let peers = init(&mut cell, total_nodes).await;
+
+    tracing::trace!("initialization done");
+
+    follower(
+        &mut cell,
+        &mut log,
+        &mut commit_index,
+        &mut last_applied,
+        &mut current_term,
+        &mut voted_for,
+    )
+    .await;
 }
 
 // this function is not part of the raft protocol,
@@ -23,7 +53,7 @@ where
 // since they are memory addresses, we can't know them in advance,
 // when actum will switch to a different type of actor reference like a network address,
 // this function can be made to read from a file the addresses of the other servers instead
-async fn init<AB>(mut cell: AB, total_nodes: usize) -> Vec<ActorRef<RaftMessage>>
+async fn init<AB>(cell: &mut AB, total_nodes: usize) -> Vec<ActorRef<RaftMessage>>
 where
     AB: ActorBounds<RaftMessage>,
 {
@@ -37,10 +67,10 @@ where
                 RaftMessage::AddPeer(peer) => {
                     npeers += 1;
                     peers.push(peer);
-                    tracing::trace!("Peer added, total: {}", npeers);
+                    tracing::trace!("➕ Peer added, total: {}", npeers);
                 }
                 _ => {
-                    tracing::warn!("Received a message that is not AddPeer, ignoring, this should not happen");
+                    tracing::warn!("❔ Received a message that is not AddPeer, ignoring, this should not happen");
                 }
             },
             None => {
@@ -53,7 +83,48 @@ where
     peers
 }
 
-async fn follower() {}
+// follower nodes receive AppendEntry messages from the leader and execute them
+// they ping the leader to see if it's still alive, if it isn't, they start an election
+async fn follower<AB>(
+    cell: &mut AB,
+    log: &mut Vec<Message>,
+    commit_index: &mut usize,
+    last_applied: &mut usize,
+    current_term: &mut u64,
+    voted_for: &mut Option<ActorRef<RaftMessage>>,
+) where
+    AB: ActorBounds<RaftMessage>,
+{
+    let max_time_before_election = Duration::from_secs(10);
+    
+    loop {
+        let wait_res = timeout(max_time_before_election, cell.recv()).await;
+
+        match wait_res {
+            Ok(received_message) => match received_message.message() {
+                Some(raftmessage) => match raftmessage {
+                    RaftMessage::AppendEntries(mut entries) => {
+                        tracing::info!("✏️ Received an AppendEntries message, adding them to the log");
+                        log.append(&mut entries);
+                    }
+                    RaftMessage::RequestVote => {
+                        // TODO: check if we can vote for the candidate
+                    }
+                    _ => {
+                        tracing::warn!("❔ Received a message that is not AppendEntry or RequestVote while in follower mode, ignoring");
+                    }
+                },
+                None => {
+                    tracing::info!("Received a None message, quitting");
+                    break;
+                }
+            },
+            Err(_) => {
+                tracing::info!("⏰ Timeout reached, starting an election (or at least pretending to)");
+            }
+        }
+    }
+}
 
 async fn candidate() {}
 
