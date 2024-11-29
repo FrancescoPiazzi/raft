498260c754f668c889641858cb28dc54778a1f34
FrancescoPiazzi <francescopiazzi25@gmail.com>
2024-11-29 12:01:40 +0100

    removed useless trait

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,10 @@
-use crate::common_state::CommonState;
-use crate::messages::TermProvider;
+use crate::{common_state::CommonState, messages::RaftMessage};
 
 /// Updates current term if necessary, returns true if we have to become a follower, false otherwise
-pub fn update_term<TP, SM, SMin, SMout>(common_state: &mut CommonState<SM, SMin, SMout>, msg: &TP) -> bool
-where 
-TP: TermProvider
+pub fn update_term<SM, SMin, SMout>(
+    common_state: &mut CommonState<SM, SMin, SMout>, 
+    msg: &RaftMessage<SMin, SMout>
+) -> bool 
 {
     if let Some(inner) = msg.get_term(){
         if inner > common_state.current_term {
--- src/messages.rs
+++ src/messages.rs
@@ -10,13 +10,6 @@ use crate::messages::append_entries::{AppendEntriesReply, AppendEntriesRequest};
 use crate::messages::append_entries_client::AppendEntriesClientRequest;
 use crate::messages::request_vote::{RequestVoteReply, RequestVoteRequest};
 
-/// Every message exchanged that strictly belongs to the Raft algorithm has a term
-/// this trait is used to get the term of a message I don't know the type of
-/// the optional wrapper allows for types that aren't exactly part of the algorithm
-/// like AddPeer and AppendEntriesClientRequest to return cleanly
-pub(crate) trait TermProvider{
-    fn get_term(&self) -> Option<u64>;
-}
 
 pub enum RaftMessage<SMin, SMout> {
     AddPeer(AddPeer<SMin, SMout>),
@@ -25,6 +18,19 @@ pub enum RaftMessage<SMin, SMout> {
     RequestVoteRequest(RequestVoteRequest),
     RequestVoteReply(RequestVoteReply),
     AppendEntriesClientRequest(AppendEntriesClientRequest<SMin, SMout>),
+}
+
+impl<SMin, SMout> RaftMessage<SMin, SMout> {
+    pub fn get_term(&self) -> Option<u64> {
+        match self {
+            Self::AddPeer(_) => None,
+            Self::AppendEntriesClientRequest(_) => None,
+            Self::AppendEntriesRequest(inner) => Some(inner.term),
+            Self::AppendEntriesReply(inner) => Some(inner.term),
+            Self::RequestVoteRequest(inner) => Some(inner.term),
+            Self::RequestVoteReply(inner) => Some(inner.term),
+        }
+    }
 }
 
 impl<SMin, SMout> Debug for RaftMessage<SMin, SMout> {
@@ -73,18 +79,5 @@ impl<SMin, SMout> From<RequestVoteReply> for RaftMessage<SMin, SMout> {
 impl<SMin, SMout> From<AppendEntriesClientRequest<SMin, SMout>> for RaftMessage<SMin, SMout> {
     fn from(value: AppendEntriesClientRequest<SMin, SMout>) -> Self {
         Self::AppendEntriesClientRequest(value)
-    }
-}
-
-impl<SMin, SMout> TermProvider for RaftMessage<SMin, SMout> {
-    fn get_term(&self) -> Option<u64> {
-        match &self {
-            RaftMessage::AddPeer(_) => {None}
-            RaftMessage::AppendEntriesClientRequest(_) => None,
-            RaftMessage::AppendEntriesReply(msg) => Some(msg.term),
-            RaftMessage::AppendEntriesRequest(msg) => Some(msg.term),
-            RaftMessage::RequestVoteRequest(msg) => Some(msg.term),
-            RaftMessage::RequestVoteReply(msg) => Some(msg.term),
-        }
     }
 }
