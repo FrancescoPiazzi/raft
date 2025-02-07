398447d750b6c869c4001fbdd9e45076c857c6c7
andreastedile <andrea.stedile@studenti.unitn.it>
2025-02-07 18:37:34 +0100

    refactor code for readability

--- tests/dropped_messages_replication.rs
+++ tests/dropped_messages_replication.rs
@@ -13,22 +13,26 @@ mod test_state_machine;
 use crate::test_state_machine::TestStateMachine;
 use oxidized_float::util::run_testkit_until_actor_returns;
 
-#[tokio::test]
-async fn dropped_messages_replication() {
-    tracing_subscriber::fmt()
-        .with_span_events(
-            tracing_subscriber::fmt::format::FmtSpan::NEW | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
-        )
-        .with_target(false)
-        .with_line_number(true)
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+const USE_FILE_LOGS: bool = false;
 
-    let n_servers = 5;
-    let time_to_elect_leader = Duration::from_millis(1000);
-    let time_to_agree_on_value = Duration::from_millis(1000);
+async fn dropped_messages_replication(
+    n_servers: usize,
+    time_to_elect_leader: Duration,
+    time_to_replicate_entry: Duration,
+) {
+    let guards;
+    if USE_FILE_LOGS {
+        guards = split_file_logs(n_servers);
+    } else {
+        tracing_subscriber::fmt()
+            .with_span_events(tracing_subscriber::fmt::format::FmtSpan::NONE)
+            .with_target(false)
+            .with_line_number(false)
+            .with_max_level(tracing::Level::TRACE)
+            .init();
+    }
 
-    // use the utility function to create all the followers with one call
+    // spawn all followers with the utility function
     let SplitServersWithTestkit {
         mut server_id_vec,
         mut server_ref_vec,
@@ -43,9 +47,9 @@ async fn dropped_messages_replication() {
         Some(n_servers),
     );
 
-    // manually create the leader
+    // manually create and spawn the leader
     let leader_id = (n_servers - 1) as u32;
-    let (actor, tk) = testkit(move |cell, _| async move {
+    let (leader, leader_tk) = testkit(move |cell, _| async move {
         raft_server(
             cell,
             leader_id,
@@ -56,16 +60,16 @@ async fn dropped_messages_replication() {
         )
         .await
     });
-    let handle = tokio::spawn(actor.task.run_task().instrument(info_span!("leader")));
-    let mut leader_ref = actor.m_ref.clone(); // useful to contact the leader later
+    let handle = tokio::spawn(leader.task.run_task().instrument(info_span!("leader")));
+    let mut leader_ref = leader.m_ref; // useful to contact the leader later
 
-    server_ref_vec.push(actor.m_ref);
+    server_ref_vec.push(leader_ref.clone());
     server_id_vec.push(leader_id);
     handle_vec.push(handle);
-    guard_vec.push(actor.guard);
-    testkit_vec.push(tk);
+    guard_vec.push(leader.guard);
+    testkit_vec.push(leader_tk);
 
-    let mut testkit_handles = Vec::with_capacity(testkit_vec.len());
+    let mut testkit_handles = Vec::with_capacity(n_servers);
     for testkit in testkit_vec {
         let handle = tokio::spawn(run_testkit_until_actor_returns(testkit));
         testkit_handles.push(handle);
@@ -75,30 +79,33 @@ async fn dropped_messages_replication() {
 
     let (tx, mut rx) = mpsc::channel::<AppendEntriesClientResponse<u64, usize>>(10);
 
+    tracing::debug!("sleeping for {}, so that a leader can be elected", time_to_elect_leader.as_secs());
     sleep(time_to_elect_leader).await;
 
-    tracing::debug!("Sending request");
-
+    tracing::debug!("sending AppendEntriesClientRequest to leader");
     let msg = AppendEntriesClientRequest {
         reply_to: tx,
         entries_to_replicate: vec![1],
     };
     leader_ref.try_send(msg.into()).unwrap();
 
-    sleep(time_to_agree_on_value).await;
+    tracing::debug!("sleeping for {}, so that servers can the entry can be replicated", time_to_replicate_entry.as_secs());
+    sleep(time_to_replicate_entry).await;
 
     let recv = rx.recv().await.unwrap();
     assert!(matches!(recv, AppendEntriesClientResponse(Ok(1))));
 
-    tracing::debug!("Response ok");
+    tracing::debug!("dropping actor guards");
+    for guard in guard_vec {
+        drop(guard);
+    }
 
-    let _ = guard_vec.into_iter().for_each(|g| drop(g));
-    let mut state_machines: Vec<TestStateMachine> = Vec::new();
+    tracing::debug!("waiting for servers to return and collecting their state machines");
+    let mut state_machines = Vec::<TestStateMachine>::with_capacity(n_servers);
     for (i, handle) in handle_vec.into_iter().rev().enumerate() {
-        tracing::debug!("Waiting for server {} to return", i);
+        tracing::debug!("waiting for server {} to return", i);
         state_machines.push(handle.await.unwrap());
-        tracing::debug!("Server {} returned", i);
-        // sleep(Duration::from_millis(200)).await;
+        tracing::debug!("server {} returned", i);
     }
 
     // very loose check, but if it passes at least something has gone right
@@ -106,9 +113,15 @@ async fn dropped_messages_replication() {
         assert_eq!(state_machines[i].set.len(), 1);
     }
 
+    tracing::debug!("waiting for testkits to return");
     for handle in testkit_handles.into_iter() {
         handle
             .await
             .expect("the testkit loop should return after that the actor has returned");
     }
+}
+
+#[tokio::test]
+async fn dropped_messages_replication_wrap() {
+    dropped_messages_replication(5, Duration::from_millis(1000), Duration::from_millis(1000)).await;
 }
